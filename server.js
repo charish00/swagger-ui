070122f125e4b7745ca962dd08a57c478916ff70
panxiao <panxiao@linklogis.com>
2019-12-16 17:44:48 +0800

    体验优化

--- server.js
+++ server.js
@@ -1,90 +1,62 @@
 #!/usr/bin/env node
-<<<<<<< HEAD
-// swaggerproxy
-// node server.js --swagger_url https://llsams.qhhrly.cn/v2/api-docs --proxy_url https://llsams.qhhrly.cn --sso_url https://lls-sso.qhhrly.cn --username wby --password 1
-
-=======
 
 const argv = require('yargs').argv
->>>>>>> 44f60f3733c24b5eb2190f8a0edaf3476835b45a
 const path = require('path');
 const Url = require('url-parse');
 const express = require('express');
 const axios = require('axios');
 const proxy = require('http-proxy-middleware');
+const opn = require('opn');
 
 console.log('argv is: ' + JSON.stringify(argv));
 
 const app = express();
 
-let { swagger_url, proxy_url, listen_to, sso_url, username, password } = argv;
+let { swagger_url, proxy_url, listen_to } = argv;
 listen_to = listen_to || 9000;
 
 if (!swagger_url) {
-    console.error("swagger_url not set, please start with 'swaggerproxy --swagger_url xxx --proxy_url xxx --sso_url xxx --username xxx --password xxx' ");
+    console.error("swagger_url not set, please start with 'swaggerproxy --swagger_url xxx --proxy_url xxx' ");
     return;
 }
 if (!proxy_url) {
-    console.error("proxy_url not set, please start with 'swaggerproxy --swagger_url xxx --proxy_url xxx --sso_url xxx --username xxx --password xxx' ");
+    console.error("proxy_url not set, please start with 'swaggerproxy --swagger_url xxx --proxy_url xxx' ");
     return;
 }
 
 const surl = new Url(swagger_url);
 const purl = new Url(proxy_url);
 
-let swagger_content;
 axios.get(swagger_url).then(res => {
-    swagger_content = res.data;
-    swagger_content.host = 'localhost:'+listen_to;
+    let swagger_content = res.data;
+    swagger_content.host = 'localhost:' + listen_to;
     swagger_content = JSON.stringify(swagger_content);
-})
 
-if (!sso_url) {
-    startServer();
-    return;
-}
-
-if (!username) {
-    console.error("username not set, please start with 'swaggerproxy --swagger_url xxx --proxy_url xxx --sso_url xxx --username xxx --password xxx' ");
-    return;
-}
-if (!password) {
-    console.error("password not set, please start with 'swaggerproxy --swagger_url xxx --proxy_url xxx --sso_url xxx --username xxx --password xxx' ");
-    return;
-}
-
-axios.get(`${sso_url}/sso-web/doLogin?username=${username}&password=${password}&ifRemember=true`).then(response => {
-    if (response.status != 200) {
-        console.error(response.statusText);
-        return;
-    }
-    if (response.data.code != '200') {
-        console.error(response.data.message);
-        return;
-    }
-    startServer(response.headers['set-cookie']);
-});
-
-function startServer(cookies) {
     app.use('/swagger-ui/env.js', (req, res) => {
-        if (cookies) {
-            cookies.forEach(cookie => {
-                let cookieArr = cookie.split(';')[0].split('=');
-                res.cookie(cookieArr[0], cookieArr[1] || '');
-            });
-        }
-        res.send('(function(){window.swagger_api_docs="'+surl.pathname+surl.query+'"})()')
+        res.send(`(function(){window.swagger_api_docs="${surl.pathname + surl.query}"})()`)
     });
     app.use('/swagger-ui', express.static(path.join(__dirname, 'frontend')));
     app.use(surl.pathname, (req, res) => res.send(swagger_content));
-    app.use('/', proxy({ 
-        target: purl.origin, 
+    app.use('/', proxy({
+        target: purl.origin,
         pathRewrite: {
             '/': purl.pathname
         },
-        changeOrigin: true 
+        changeOrigin: true,
+        onProxyReq(proxyReq, req, res) {
+            let authCode = req.headers.authcode;
+            if (authCode) {
+                proxyReq.setHeader('auth-code', authCode);
+                req.headers['auth-code'] = authCode;
+            }
+        }
     }));
     app.listen(listen_to);
 
-    console.log('proxy server start up: http://localhost:' + listen_to + '/swagger-ui/index.html');
-}
+    const loginview = `http://localhost:${listen_to}/index.html`;
+    const swaggerView = `http://localhost:${listen_to}/swagger-ui/index.html`;
+    opn(loginview, { app: 'chrome' });
+    opn(swaggerView, { app: 'chrome' }).then(() => {
+        console.log(swaggerView);
+    });
+})
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import SwaggerUI from 'swagger-ui'
-import 'swagger-ui/dist/swagger-ui.css'
-
-SwaggerUI({
-  dom_id: '#myDomId',
-  url: swagger_api_docs
-})
